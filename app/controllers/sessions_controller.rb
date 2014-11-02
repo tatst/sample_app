bb5cdfdf8d4ea83ad76169ef5e23fbb4cfa7567c
Tatsuya Toyoda <b8eansatz@gmail.com>
2014-11-02 16:21:59 +0900

    list 8.10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,12 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+    else
+      flash[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
