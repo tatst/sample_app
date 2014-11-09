1e83dbef6783f657756f5f428746721bb2250ce3
Tatsuya Toyoda <b8eansatz@gmail.com>
2014-11-09 16:48:39 +0900

    list 11.24

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ SampleApp::Application.routes.draw do
     end
   end
 
-  resources :sessions,   only: [:new, :create, :destroy]
-  resources :microposts, only: [:create, :destroy]
+  resources :sessions,	    only: [:new, :create, :destroy]
+  resources :microposts,    only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root 'static_pages#home'
   match '/signup',  to: 'users#new',		via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
